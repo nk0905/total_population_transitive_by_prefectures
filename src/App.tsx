b715dffc99aa126cfc0fc772821192ec9036798b
nk0905 <0dd2n.a0905@gmail.com>
2024-06-04 22:52:55 +0900

    Loadingコンポーネント作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './wdyr';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import PopulationTransition from './pages/PopulationTransition.page';
+import { LoadingProvider } from './contexts/Loading.context';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/population-transition" />} />
-      <Route path="/population-transition" element={<PopulationTransition />} />
-    </Routes>
+    <LoadingProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to="/population-transition" />} />
+        <Route path="/population-transition" element={<PopulationTransition />} />
+      </Routes>
+    </LoadingProvider>
   );
 }
 
